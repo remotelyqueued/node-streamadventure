7872aeeeec2c39809c016b529352cdc690b5b906
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-27 22:47:48 -0700

    Finished concat

--- 8-concat.js
+++ 8-concat.js
@@ -2,9 +2,10 @@
 // convert buffer to string and reverse it
 // send to process.stdout
 
-const data = [];
-process.stdin
-    .on('data', function (chunk) {
-        data.push(chunk.toString().split('').reverse().join(''));
+const concat = require('concat-stream');
+
+process.stdin.pipe(
+    concat(function (data) {
+        process.stdout.write(data.toString().split('').reverse().join(''));
     })
-    .pipe(process.stdout);
+);
