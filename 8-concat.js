9c53f82ee3e1c5acb734ed8eea1d56e3f13ac1cc
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-09-27 23:11:53 -0700

    Created solution without NPM concat

--- 8-concat.js
+++ 8-concat.js
@@ -1,11 +1,22 @@
-// read from process.stdin
-// convert buffer to string and reverse it
-// send to process.stdout
+let body = '';
+process.stdin.on('data', chunk => (body += chunk));
+process.stdin.on('end', () => {
+    process.stdout.write(body.split('').reverse().join(''));
+});
 
-const concat = require('concat-stream');
+// 1.
+// const concat = require('concat-stream');
+// process.stdin.pipe(
+//     concat(function (data) {
+//         process.stdout.write(data.toString().split('').reverse().join(''));
+//     })
+// );
 
-process.stdin.pipe(
-    concat(function (data) {
-        process.stdout.write(data.toString().split('').reverse().join(''));
-    })
-);
+// oficial solution
+// const concat = require('concat-stream');
+// process.stdin.pipe(
+//     concat(function (src) {
+//         const s = src.toString().split('').reverse().join('');
+//         process.stdout.write(s);
+//     })
+// );
